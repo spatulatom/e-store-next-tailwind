4242327b89fa900f5e978653928e1e892335681d
spatulatom <spatulatom@gmail.com>
2022-11-26 21:10:16 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>test</div>
+  )
+}
+
+
+export default function test() {
+  return <div>test</div>;
+}
